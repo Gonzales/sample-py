e04211a0fe7f0b6f04a5eb888f8872cacd25db72
Gokhan Karasoglu <>
2021-08-01 20:26:24 +0000

    cleanup

--- app/app.py
+++ app/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, jsonify
-from service import ToDoService
-from models import Schema
 
 import json
 
@@ -11,7 +9,7 @@ app = Flask(__name__)
 def add_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
+    response.headers['Access-Control-Allow-Methods']=  "GET"
     return response
 
 
@@ -20,31 +18,7 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/<name>")
-def hello_name(name):
-    return "Hello " + name
-
-
-@app.route("/todo", methods=["GET"])
-def list_todo():
-    return jsonify(ToDoService().list())
-
-
-@app.route("/todo", methods=["POST"])
-def create_todo():
-    return jsonify(ToDoService().create(request.get_json()))
-
-
-@app.route("/todo/<item_id>", methods=["PUT"])
-def update_item(item_id):
-    return jsonify(ToDoService().update(item_id, request.get_json()))
-
-
-@app.route("/todo/<item_id>", methods=["DELETE"])
-def delete_item(item_id):
-    return jsonify(ToDoService().delete(item_id))
-
 
 if __name__ == "__main__":
-    Schema()
+
     app.run(debug=True, host='0.0.0.0', port=8888)
